34e23339eeb7075f1de01a21353cddd1aa359f17
hanwoo726 <hanwoo726>
2025-09-30 16:41:34 +0900

    rename

--- src/main/java/com/lec/spring/domain/User.java
+++ src/main/java/com/lec/spring/domain/User.java
@@ -16,6 +16,8 @@ public class User{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Long ad;
+
     @Column(unique = true, nullable = false)
     private String username;
     @Column(nullable = false)
